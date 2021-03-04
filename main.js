ba40ceba1ff6b0e81216053f0fad83c729ba7b43
jeremy barbosa <jeremybarbosa@jeremys-mbp.home>
2021-03-04 13:58:30 -0600

    donzo

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 function numberDoubler(num) {
-    // Your code here
+    while(num<100){
+        num *=2;
+    }
+    return num;
 }
 
 function stringRepeater(str) {
-    // Your code here
+    str1= '';
+    while(str1.length < 10){
+        str1 +=str;
+    }
+    return str1;
 }
 
 function makeDivisible(x, y) {
-    // Your code here
+    while(x % y !== 0){
+        x += 1;
+    }
+    return x;
 }
 
 module.exports = {
